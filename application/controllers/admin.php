1663759f76e3c3bb5fae75842917efbe86a981dc
Adyputra <adynugrahaputra@gmail.com>
2021-06-12 21:19:49 +0700

    Gejala

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -21,17 +21,81 @@ class admin extends CI_Controller
 
 	public function gejala()
 	{
+		$id_admin = $this->session->userdata('id_admin');
+
+		$db = $this->model_admin->get_admin($id_admin)->row();
+		$data['nama_admin'] = $db->nama_admin;
+		$data['gejala'] = $this->model_admin->get_all_gejala()->result_array();
+
+		$this->load->view('admin/navbar', $data);
+		$this->load->view('admin/sidebar', $data);
+		$this->load->view('admin/gejala', $data);
+		$this->load->view('admin/footer');
 	}
 
 	public function add_gejala()
 	{
+		$add = $this->model_admin->add_gejala();
+
+		if ($add['result'] == 'success') {
+
+			$this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+				Add Gejala Success!
+			</div>
+			');
+			redirect('admin/gejala');
+		} else {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+				Add Gejala Failed!
+			</div>
+			');
+			redirect('admin/gejala');
+		}
 	}
 
 	public function delete_gejala()
 	{
+
+		$delete = $this->model_admin->delete_gejala();
+
+		if ($delete['result'] == 'success') {
+
+			$this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+				Delete Gejala Success!
+			</div>
+			');
+			redirect('admin/gejala');
+		} else {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+				Delete Gejala Failed!
+			</div>
+			');
+			redirect('admin/gejala');
+		}
 	}
+
 	public function edit_gejala()
 	{
+
+		$delete = $this->model_admin->edit_gejala();
+
+
+
+		if ($delete['result'] == 'success') {
+
+			$this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+				Edit Gejala Success!
+			</div>
+			');
+
+
+			redirect('admin/gejala');
+		} else {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+				Edit Gejala Failed!
+			</div>
+			');
+		}
 	}
 
 	public function hamapenyakit()
--- application/models/model_admin.php
+++ application/models/model_admin.php
@@ -1,88 +1,123 @@
-<?php 
+<?php
+
 /**
-* 
-*/
+ * 
+ */
 class model_admin extends CI_Model
-{	
+{
 
 	function get_admin($id_admin)
 	{
-		
 	}
 
 	function get_all_admin()
 	{
-		
-		
 	}
 
 	function edit_admin()
 	{
-
 	}
 
-	function get_all_gejala(){
-		
-		
+	function get_all_gejala()
+	{
+
+		$this->db->select('*');
+		$this->db->from('gejala');
+
+		return $this->db->get();
 	}
-	function get_hp($kode_hp){
-		
-	
+
+	function get_hp($kode_hp)
+	{
 	}
-	
+
 	function add_gejala()
 	{
 
+		$this->db->order_by('kode_gejala', 'Desc');
+		$db = $this->db->get('gejala')->row();
+
+		$noUrut = (int) substr($db->kode_gejala, 1);
+		$noUrut++;
+
+		$char = "G";
+		$newID = $char . sprintf("%02s", $noUrut);
+
+		$data = array(
+			'kode_gejala' => $newID,
+			'gejala' =>  $this->input->post('gejala')
+		);
+
+		$this->db->insert('gejala', $data);
+
+		if ($this->db->affected_rows() > 0) {
+			$return =  array('result' => 'success');
+		} else {
+			$return =  array('result' => 'failed');
+		}
+		return $return;
 	}
 
 	function delete_gejala()
 	{
 
+		$this->db->where('kode_gejala', $this->input->post('kode_gejala'));
+		$this->db->delete('gejala');
+
+		if ($this->db->affected_rows() > 0) {
+			$return =  array('result' => 'success');
+		} else {
+			$return =  array('result' => 'failed');
+		}
+		return $return;
 	}
 
 	function edit_gejala()
 	{
 
+		$data = array(
+			'gejala' =>  $this->input->post('gejala')
+		);
+		$this->db->where('kode_gejala', $this->input->post('kode_gejala'));
+		$this->db->update('gejala', $data);
+
+		if ($this->db->affected_rows() > 0) {
+			$return =  array('result' => 'success');
+		} else {
+			$return =  array('result' => 'failed');
+		}
+		return $return;
 	}
 
 	function get_all_hamapenyakit()
 	{
-		
 	}
-	
+
 	function add_hamapenyakit()
 	{
-
 	}
 
 	function delete_hamapenyakit()
 	{
-
 	}
 
 	function edit_hamapenyakit()
 	{
-
-
 	}
 
 	function get_gejala_hp($kode_hp)
 	{
-			
 	}
-	
+
 	function add_basispengetahuan()
 	{
-
 	}
 
 	function delete_basispengetahuan()
 	{
-
 	}
 
 	function edit_basispengetahuan()
 	{
-
 	}
 }
